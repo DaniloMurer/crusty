414596ba4703e017d88beddc7a3c4b7ea18e8c2d
Danilo Jakob <danilojakob@protonmail.com>
2024-09-06 21:27:52 +0200

    feat: refactored a bit based on raylibrs examples, especially for code better code structure

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,55 @@
-use raylib::{color::Color, ffi::KeyboardKey, math::Rectangle, prelude::RaylibDraw, RaylibHandle};
+use raylib::{color::Color, ffi::{KeyboardKey, Vector2}, prelude::RaylibDraw, RaylibHandle, RaylibThread};
+
+struct Player {
+    position: Vector2,
+    size: Vector2,
+    color: Color
+}
+
+struct Game {
+    player: Player
+}
+
+impl Game {
+    fn default() -> Game {
+        Game{player: Player { position: Vector2 { x: 400.0, y: 400.0 }, size: Vector2 { x: 100.0, y: 50.0 }, color: Color::RED }}
+    }
+}
 
 fn main() {
-    // this bad boy here is our 'player'
-    let mut rect = Rectangle::new(400.0, 400.0, 100.0, 50.0);
-
     // initialize raylib and create a window
     let (mut rl, thread) = raylib::init()
         .size(800, 600)
         .title("Crusty")
         .build();
-
-    // limit the framerate to 250 fps because else inputs will go bonkers
     rl.set_target_fps(250);
+    let mut game = Game::default();
     while !rl.window_should_close() {
         // handle keyboard input for our player
-        handle_input_rectangle(&mut rl, &mut rect);
-
-        let mut draw = rl.begin_drawing(&thread);
-        draw.draw_fps(700, 500);
-        draw.clear_background(Color::PURPLE);
-        draw.draw_text("Hello Crusty", 350, 300, 20, Color::WHITE);
-        //draw our player
-        draw.draw_rectangle_rec(rect, Color::RED);
+        update_game(&mut rl, &mut game);
+        draw_game(&mut rl, &thread, &mut game);
     }
 }
 
-/// handle keyboard input for our player and update rectangle position accordingly
-fn handle_input_rectangle(rl: &mut RaylibHandle, rect: &mut Rectangle) {
+fn update_game(rl: &mut RaylibHandle, game: &mut Game) {
     if rl.is_key_down(KeyboardKey::KEY_UP) {
-        rect.y -= 1.0;
+        game.player.position.y -= 1.0;
     } else if rl.is_key_down(KeyboardKey::KEY_DOWN) {
-        rect.y += 1.0;
+        game.player.position.y += 1.0;
     } else if rl.is_key_down(KeyboardKey::KEY_LEFT) {
-        rect.x -= 1.0;
+        game.player.position.x -= 1.0;
     } else if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
-        rect.x += 1.0;
+        game.player.position.x += 1.0;
     }
+}
+
+fn draw_game(rl: &mut RaylibHandle, thread: &RaylibThread, game: &mut Game) {
+
+    let mut draw = rl.begin_drawing(thread);
+    draw.draw_fps(700, 500);
+    draw.clear_background(Color::PURPLE);
+    draw.draw_text("Hello Crusty", 350, 300, 20, Color::WHITE);
+    //draw our player
+    draw.draw_rectangle_v(game.player.position, game.player.size, game.player.color);
+
 }
